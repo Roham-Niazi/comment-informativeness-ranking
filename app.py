47f8e6c47ff8b512d4845c4a8d515268daf0cc7e
Roham Niazi <Roham.n24@gmail.com>
2026-08-31 17:28:15 +0330

    Updating dependencies and Fixing bugs

--- app.py
+++ app.py
@@ -69,8 +69,8 @@ with gr.Blocks() as demo:
 
 
 
+port=int(os.environ.get("PORT", 7860))
 demo.launch(
 	server_name="0.0.0.0",
-	server_port=int(os.environ.get("PORT", 7860)),
-	share=False
+	server_port=port,
 )
