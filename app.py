d68d3cda7df675ab2ea3646de66022fa6b54809f
Roham Niazi <Roham.n24@gmail.com>
2026-09-15 17:06:17 +0430

    Updating gitignore & Fixing bugs

--- app.py
+++ app.py
@@ -69,8 +69,5 @@ with gr.Blocks(title="Comment Informativeness Ranker") as demo:
 
 
 
-port=int(os.environ.get("PORT", 7860))
-demo.launch(
-	server_name="0.0.0.0",
-	server_port=port,
-)
+
+demo.launch()
